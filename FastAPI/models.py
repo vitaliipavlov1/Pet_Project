086bf9866dd07c9750ae0332fdd868d29069daa5
vitaliipavlov1 <pavlov.v.mail1@gmail.com>
2025-07-20 21:47:45 +0200

    Create models.py

---
+++ FastAPI/models.py
@@ -0,0 +1,23 @@
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, LargeBinary
+from sqlalchemy.orm import relationship
+from .database import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    phone_number = Column(Integer, unique=True, index=True, nullable=False)
+    is_active = Column(Boolean, default=True)
+
+    images = relationship("Images", back_populates="user")
+
+class Images(Base):
+    __tablename__ = "images"
+
+    id = Column(Integer, primary_key=True, index=True)
+    phone_number = Column(Integer, ForeignKey("users.phone_number"))
+    real_image = Column(LargeBinary, nullable=False)
+    heatmap_gen = Column(LargeBinary, nullable=False)
+    heatmap_dis = Column(LargeBinary, nullable=False)
+
+    user = relationship("User", back_populates="images")
